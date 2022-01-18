a95578fd1bd6f0de6d52ae49809b04c2060f4f4d
Riddhi-22 <riddhikohli41@gmail.com>
2022-01-18 14:01:35 +0530

    React setup addded

--- router/user.js
+++ router/user.js
@@ -5,7 +5,11 @@ const router = express.Router();
 
 router.get("/login", (req, res) => {
     res.send("Login in user");
-})
+});
+
+router.get("/blogs", (req, res) => {
+    res.send("Blogs");
+});
 
 module.exports = router;
 
